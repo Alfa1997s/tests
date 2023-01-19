c03811bfdd2f7773288369ba6d43fc47e86ad891
Alfa1997com <alamptey007@st.ug.edu.gh>
2023-01-19 10:43:46 +0100

    setting a variable for the local host

--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 import unittest
 import functions
 import requests
-
+default_server = 'http://127.0.0.1:8000'
 class pms_functions(unittest.TestCase):
+    
      def test_leaked(self):
-          home = 'http://127.0.0.1:8000'
+          home = default_server
           response = requests.get(f'{home}/check_leak', {
                'password': 'mylestone1897'
           })
@@ -13,7 +14,7 @@ class pms_functions(unittest.TestCase):
          
     
      def test_Password_length(self):
-       home = 'http://127.0.0.1:8000'
+   
        params = {'password': 'mylestone1897', 'min_length': 8, 'max_length': 50}
        response = requests.get(f'{home}/check_length', params=params)
        self.assertEqual(response.status_code, 200)
@@ -21,8 +22,8 @@ class pms_functions(unittest.TestCase):
  
           
      def test_special_characters(self):
-          home = 'http://127.0.0.1:8000'
-          passwordLst = ["!","@","$","&"]
+       
+          password_lst = ["!","@","$","&"]
           params = {'password':'mylestone@1897'}
           response = requests.get(f'{home}/check_specialcharacters',params=params)
           self.assertEqual(response.status_code, 200)
@@ -31,12 +32,10 @@ class pms_functions(unittest.TestCase):
 
      
      def test_update_policy(self):
-          home = 'http://127.0.0.1:8000'
           polices_dct={"minLength":8,"maxLength":50,"specialCharacters":1,"upperCase":1,"lowerCase":1}
           self.assertEqual(functions.updatePolicy("minLength",10),"Update Success")
 
      def test_genPass(self):
-          home = 'http://127.0.0.1:8000'
           response = requests.get(f'{home}/generatePassword')
           self.assertEqual(response.status_code,200) 
           
