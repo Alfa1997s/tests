69cfc4d270474cf47c9b7b8e3c9824304f847b6e
Alfa1997com <alamptey007@st.ug.edu.gh>
2023-01-19 10:39:23 +0100

    more

--- functions.py
+++ functions.py
@@ -39,17 +39,17 @@ def create_account(user,password):
 
 @app.get('/check_specialcharacters')
 def special_characters(password):
-    passwordLst = ["!","@","$","&"]
+    password_lst = ["!","@","$","&"]
     for char in password:
-        if char in passwordLst:
+        if char in password_lst:
             return "ok"
     return "Password must contain at least one special character"
 
 @app.get('/update_policy')
-def update_policy(policyName,policyValue):
+def update_policy(policy_name,policy_value):
     polices_dct={"minLength":8,"maxLength":50,"specialCharacters":1,"upperCase":1,"lowerCase":1}
-    if policyName in polices_dct:
-        polices_dct[policyName]=policyValue
+    if policy_name in polices_dct:
+        polices_dct[policy_name]=policy_value
         return "Update Success" 
     else:
         return "Policy Not Found" 
@@ -58,22 +58,22 @@ def update_policy(policyName,policyValue):
 def generate_random_password():
     max_length=50
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    alphabets_lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 
+    alphabets_lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 
                             'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y',
                             'z']
 
-    alphabets_upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q','R', 
+    alphabets_uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q','R', 
                               'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
 
     special_characters = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|',
                                '~', '>','*', '(', ')', '<']
 
-    all_combined = numbers + alphabets_lowerCase + alphabets_upperCase + special_characters
+    all_combined = numbers + alphabets_lowercase + alphabets_uppercase + special_characters
 
     # Get Random characters from number, uppercase alphabets, lowercase alphabets, special charcters
     random_numbers = random.choice(numbers)
-    random_upper = random.choice(alphabets_upperCase)
-    random_lower = random.choice(alphabets_lowerCase)
+    random_upper = random.choice(alphabets_uppercase)
+    random_lower = random.choice(alphabets_lowercase)
     random_special_char = random.choice(special_characters)
 
     temp = random_numbers + random_upper + random_lower + random_special_char
